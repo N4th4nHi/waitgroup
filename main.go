e86351494f1fb565e20bf18cf213f82d7481fa02
N4th4nHi <2020151968@ub.edu.bz>
2023-02-15 13:35:31 -0600

    added the reverse feature

--- main.go
+++ main.go
@@ -13,12 +13,23 @@ func main() {
 	msg := flag.String("msg", "HOWDY, STRANGER", "the message to display")
 	num := flag.Int("num", 1, "How many times to print the message")
 	caps := flag.Bool("U", false, "Should the string be all caps")
+	reverse := flag.Bool("r", false, "reverse the string (true) ")
 	flag.Parse()
 
 	//check if the user set the flag
 	if *caps {
 		*msg = strings.ToUpper(*msg)
 	}
+///check if we should reverse the string
+if *reverse {
+	//reverse string
+	var result string
+	for _, value := range *msg {
+		result = string(value) + result
+	}
+	*msg = result
+}
+
 	for i := 0; i < *num; i++ {
 		fmt.Println(*msg)
 	}
